8f66f6a6ec139bde3639e50f01e7501bd38ffbce
Calmon958 <wm453856@gmail.com>
2023-11-17 04:44:11 +0300

    Static library

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_strncat - concatenates two strings
+ *@dest: destination
+ *@src: source
+ *@n: number of bytes used from src
+ *Return: the pointer to output string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i, j;
+i = 0;
+for (j = 0; dest[j] != '\0'; j++)
+;
+while (src[i] != '\0' && i < n)
+{
+dest[j + i] = src[i];
+ i++;
+}
+return (dest);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *_abs - absolute value of an integer
+ *@i: integer
+ *Return: absolute value
+ */
+int _abs(int i)
+{
+if (i < 0)
+i = i * -1;
+return (i);
+}
